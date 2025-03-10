c815db2984d04152d4c60014e44506a4b9bfb9f0
Divya Mogaveera <divyashekhar4444@gmail.com>
2025-03-10 15:34:34 +0530

    add logic and API call in NetWorthTracker filr

--- frontend/src/NetWorthTracker.jsx
+++ frontend/src/NetWorthTracker.jsx
@@ -1,1 +1,35 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const NwtWorthTracker = () => {
+    const [transactions, setTransactions] = useState([]);
+
+    const fetchTransactions = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/api/networth");
+            setTransactions(response.data);
+        }
+        catch (error) {
+            console.log("error fetching transactions", error);
+        }
+    };
+
+    useEffect(()=>{
+        fetchTransactions();
+    },[]);
+
+    return(
+        <>
+        <h2>Net WOrth tracker</h2>
+        <ul>
+            {transactions.map((transactions, index)=>(
+                <li key={index}>
+                    {transactions.description}- ${transactions.amount}
+                </li>
+            ))}
+        </ul>
+        </>
+    )
+};
+
+export default NwtWorthTracker;
